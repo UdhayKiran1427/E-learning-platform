a81097c220a8f318f7c03a51e438751f72d55462
UdhayKiran1427 <udhaykiranande@gmail.com>
2026-06-04 13:14:32 +0530

    api url issue fixed

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 import { getToken, setToken, setStoredUser } from './storage'
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL + '/api' }` || '/api',
+  baseURL: import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "/api",
   headers: { 'Content-Type': 'application/json' },
 })
 
